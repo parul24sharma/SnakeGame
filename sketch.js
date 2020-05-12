cfb751b743803c63c762977161002648bf2517c9
parul24sharma <47651781+parul24sharma@users.noreply.github.com>
2020-05-12 14:02:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,34 @@
-var s;
-var sizeScale=1;
-var speed = 10;
+var snake;
+// var sizeScale=1;
+var speed = 2;
 var food;
 
 
 function setup() {
   createCanvas(640, 480);
-  s=new Snake();
+  snake=new Snake();
   food=new Food();
 }
 
 function draw() {
-  // console.log("draw is running");
   background(45);
-  s.createSnake();
-  s.moveSnake();
+  snake.createSnake();
+  snake.moveSnake();
   food.makeFood();
-  // noLoop();
-  
+
 }
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-	  	s.directionOfSnake(0,-1);
+	  	snake.directionOfSnake(0,-1);
 	}
 	if(keyCode === DOWN_ARROW){
-	  	s.directionOfSnake(0,1);
+	  	snake.directionOfSnake(0,1);
 	}
 	if(keyCode === RIGHT_ARROW){
-	  	s.directionOfSnake(1,0);
+	  	snake.directionOfSnake(1,0);
 	}
 	if(keyCode === LEFT_ARROW){
-	  	s.directionOfSnake(-1,0);
+	  	snake.directionOfSnake(-1,0);
 	}
 }
 
@@ -38,37 +36,55 @@ function keyPressed(){
 function Snake(){
 	this.x =0;
 	this.y = 0;
-	this.size=30;
-	this.xspeed = 1;
-	this.yspeed = 0;
+	this.size=30;	//size of snake
+	this.xdir = 1;
+	this.ydir = 0;
 
-	this.directionOfSnake = function(x,y){
-		this.xspeed = x;
-		this.yspeed = y;
+	this.directionOfSnake = function(a,b){
+		this.xdir = a;
+		this.ydir = b;
 	}
 
 	this.moveSnake = function(){
-		this.x = this.x + this.xspeed*speed;
-		this.y = this.y + this.yspeed*speed;
+		this.x = this.x + this.xdir*speed;
+		this.y = this.y + this.ydir*speed;
 
 		this.x = constrain(this.x, 0, width-this.size);
 		this.y = constrain(this.y, 0, height-this.size);
 
+		food.isFoodEaten(this.x,this.y);
+		
 	}
 	this.createSnake = function(){
 		fill(255);
-		rect(this.x,this.y,this.size*sizeScale,this.size*sizeScale);
+		rect(this.x,this.y,this.size,this.size);
+		var snakeArray = new Array(food.foodEaten);
+		for(var i=0; i<food.foodEaten ;i++){
+			fill(255);
+			snakeArray[i] = rect(this.x-this.size*(i+1),this.y,this.size,this.size);
+		console.log(snakeArray[0]);
+		}
 	}
+	
 }
 
 function Food(){
 	this.foodX=0;
 	this.foodY=0;
-	fill("red");
-	rect(this.foodX,this.foodY,10,10);
+	this.foodSize = 30;
+	this.foodEaten = 0;
+	
+	this.isFoodEaten = function(snakeX,snakeY){
+		if(snakeX===this.foodX || snakeY===this.foodY){
+			this.foodEaten += 1;
+		}
+	}
+
+	this.foodX = floor(random(0,width-this.foodSize));
+	this.foodY = floor(random(0,height-this.foodSize));
 
 	this.makeFood = function(){
 	fill("red");
-	rect(this.foodX,this.foodY,10,10);		
+	rect(this.foodX,this.foodY,this.foodSize,this.foodSize);
 	}
 }
